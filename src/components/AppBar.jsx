b1b5b057ab14bd2c6a6e39b7b295daeaa8d2812f
Kekalainen <git@kekalainen.me>
2023-12-17 21:56:23 +0200

    Flesh out app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,16 +1,77 @@
-import { View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+
+import Text from './Text';
+import theme from '../theme';
+
+const tabs = [
+  {
+    title: 'Repositories',
+  },
+  {
+    title: 'Placeholder',
+  },
+];
+
+const topRibbonHeight = Constants.statusBarHeight;
+
+const paddingVertical = 20;
+const paddingHorizontal = 10;
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight,
-    // ...
+  topRibbon: {
+    zIndex: 1,
+    elevation: 1,
+    marginBottom: -topRibbonHeight,
+    paddingTop: topRibbonHeight,
+    backgroundColor: theme.colors.darken,
   },
-  // ...
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    paddingTop: topRibbonHeight,
+    backgroundColor: theme.colors.primary,
+  },
+  tab: {
+    paddingVertical,
+    paddingHorizontal,
+  },
 });
 
+const AppBarTab = ({ tab, isActive, setActive }) => (
+  <Pressable
+    onPress={() => setActive()}
+    style={styles.tab}
+    android_ripple={{ color: theme.colors.darken }}
+  >
+    <Text subheading light bold={isActive}>
+      {tab.title}
+    </Text>
+  </Pressable>
+);
+
 const AppBar = () => {
-  return <View style={styles.container}>{/* ... */}</View>;
+  const [activeTab, setActiveTab] = useState(tabs[0].title);
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={styles.topRibbon} />
+      <View style={styles.container}>
+        {tabs.map((tab) => (
+          <AppBarTab
+            key={tab.title}
+            tab={tab}
+            isActive={activeTab === tab.title}
+            setActive={() => setActiveTab(tab.title)}
+          />
+        ))}
+      </View>
+    </>
+  );
 };
 
 export default AppBar;
