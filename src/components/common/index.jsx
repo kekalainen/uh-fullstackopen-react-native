c235863771231e84909ddd8f75d612f35a8b5a72
Kekalainen <git@kekalainen.me>
2023-12-18 22:30:33 +0200

    Add text input components

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -1,4 +1,6 @@
 export { default as Badge } from './Badge';
 export { default as Card } from './Card';
+export { default as FormikTextInput } from './FormikTextInput';
 export { default as Stat } from './Stat';
 export { default as Text } from './Text';
+export { default as TextInput } from './TextInput';
