19662a836eab1907c19547f935416d4461b6c0e6
Kekalainen <git@kekalainen.me>
2023-12-18 23:14:26 +0200

    Implement sign-in form UI

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,41 @@
-import { Text } from './common';
+import { Formik } from 'formik';
+import { StyleSheet, View } from 'react-native';
 
-const SignIn = () => <Text>The sign-in view</Text>;
+import { Button, Card, FormikTextInput } from './common';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+});
+
+const SignInForm = ({ onSubmit }) => (
+  <>
+    <FormikTextInput name="username" placeholder="Username" />
+    <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+    <Button onPress={onSubmit}>Sign in</Button>
+  </>
+);
+
+const SignIn = () => {
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Card>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+        </Formik>
+      </Card>
+    </View>
+  );
+};
 
 export default SignIn;
