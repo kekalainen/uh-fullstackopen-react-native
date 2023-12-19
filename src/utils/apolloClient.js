6cc2c577200b523310dfbe5eb9e933780acf9ecb
Kekalainen <git@kekalainen.me>
2023-12-19 20:01:26 +0200

    Add access token to GraphQL request headers

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,4 +1,5 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Constants from 'expo-constants';
 
 const { GRAPHQL_API_URI } = Constants.manifest.extra;
@@ -11,9 +12,26 @@ const defaultOptions = {
   watchQuery: { fetchPolicy: 'cache-and-network' },
 };
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_operation, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (error) {
+      console.error(error);
+
+      return { headers };
+    }
+  });
+
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
     defaultOptions,
   });
