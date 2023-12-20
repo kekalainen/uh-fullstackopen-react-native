51eccb776b6f49adfab1c4d0991512b6f40d8d0f
Kekalainen <git@kekalainen.me>
2023-12-20 17:51:42 +0200

    Extract pure repository list container component for tests

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -14,9 +14,7 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -29,6 +27,12 @@ const RepositoryList = () => {
       style={styles.container}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
